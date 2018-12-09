b3b39c0f2e32973c44f69a06882db47c99c55446
reidwmulkey <reidwmulkey@gmail.com>
2018-12-09 12:02:45 -0600

    fixing sunday issue

--- src/main/java/com/foodtruck/finder/Main.java
+++ src/main/java/com/foodtruck/finder/Main.java
@@ -1,13 +1,20 @@
 package com.foodtruck.finder;
 
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
 public class Main {
 	public static void main(String[] args) {
 
-		FoodTrucks foodTrucks = FoodTruckRepository.getOpenFoodTrucks(new DateTime(), 0, 10);
-	
+		// convert now to the west coast time zone for determining what the
+		// current time is in san fransisco
+		DateTime now = new DateTime();
+		DateTimeZone californiaTime = DateTimeZone.forID("America/Los_Angeles");
+		now = now.withZone(californiaTime);
+
+		FoodTrucks foodTrucks = FoodTruckRepository.getOpenFoodTrucks(now, 0, 10);
+
 		System.out.println(foodTrucks.toString());
-		
+
 	}
 }
